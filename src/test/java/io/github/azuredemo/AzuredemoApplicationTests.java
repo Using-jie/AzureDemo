baa6c7c2374d72384a3dfc9661e84366cfe66d8e
use-jie <zyj@thlane.com>
2022-09-04 11:42:50 +0800

    new changes

---
+++ src/test/java/io/github/azuredemo/AzuredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.azuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
